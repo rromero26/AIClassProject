a24ff80941c5427fc4ab4b26287db52c01deb95b
Ryan Romero <32284933+rromero26@users.noreply.github.com>
2021-11-22 13:03:58 -0800

    Upload main file

---
+++ AiClassProject.py
@@ -0,0 +1,13 @@
+# AI Class Project
+# Peter Bergeon, Brian Edwards, Ryan Romero
+# Description:
+#       create a AI bot that can help navigate CSUF portal.
+
+# ----- Libraries here -----
+
+
+# ----- Helper functions -----
+
+
+# ----- Main -----
+print("Python is cool")
