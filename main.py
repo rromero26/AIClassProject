b6d8c7b5f932375acbf2cff8fa9dd3d15d841024
Ryan Romero <32284933+rromero26@users.noreply.github.com>
2021-11-30 21:51:00 -0800

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,35 @@
 # Peter Bergeon, Brian Edwards, Ryan Romero
 
 # ----- Libraries here -----
-
+import random
+import json
+import pickle
+import numpy as np
+import nltk
+from training import *
+from nltk.stem import WordNetLemmatizer
+import tensorflow
 
 
 # ----- Main -----
-print("Python is cool")
+with open("intents.json") as file:
+    data = json.load(file)
+
+print("Booting up bot. (Type quit to stop)!")
+while True:
+        userString = input("You: ")
+        if userString.lower() == "quit":
+            break
+
+        results = model.predict([bag_of_words(userString, words)])[0]
+        result_index = np.argmax(results)
+        tag = classes[result_index]
+
+        if results[result_index] > 0.7:
+            for tg in data["intents"]:
+                if tg["tag"] == tag:
+                    responses = tg['responses']
+
+            print(random.choice(responses))
+        else:
+            print("Sorry, I didn' understand. Try asking again.")
