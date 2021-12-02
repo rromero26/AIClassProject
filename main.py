147d2062b6d7a224693a065bc6b4d4615e641a8b
Ryan Romero <32284933+rromero26@users.noreply.github.com>
2021-12-01 19:11:22 -0800

    error correction and cleaning up functions

--- main.py
+++ main.py
@@ -7,30 +7,69 @@ import json
 import pickle
 import numpy as np
 import nltk
-from training import *
 from nltk.stem import WordNetLemmatizer
-import tensorflow
+from tensorflow.keras.models import load_model
 
-
-# ----- Main -----
+lemmatizer = WordNetLemmatizer()
 with open("intents.json") as file:
     data = json.load(file)
 
-print("Booting up bot. (Type quit to stop)!")
+words = pickle.load(open("words.pkl", "rb"))
+classes = pickle.load(open("classes.pkl", "rb"))
+model = load_model("chatbot_Model.h5")
+
+
+# --- Helper Functions ---
+
+# clean_up_sentences:
+#       lowercase and stems words to their root word
+def clean_up_sentences(userString):
+    sentence = nltk.word_tokenize(userString)
+    sentence = [lemmatizer.lemmatize(word.lower()) for word in sentence]
+    return sentence
+
+# bag_of_words:
+#       convert sentence to bag of words. A list of 0s and 1s that indiciate
+#       if a word exist or not in the sentence
+def bag_of_words(userString):
+    sentence = clean_up_sentences(userString)
+    bag = [0]* len(words)
+    for w in sentence:
+        for i, word in enumerate(words):
+            if word == w:
+                bag[i] = 1
+
+    return np.array(bag)
+
+# chatBot
+#       calculate the user's string a percentage value for each tag in json file
+#       we will check to see if percentage chance is greater then 70%
+#       if so, we will assume the question is that of the json tag and respond
+def chatBot(userString):
+    currentBag = bag_of_words(userString)
+    result = model.predict(np.array([currentBag]))[0]
+
+    # get the highest numerical value prediction
+    result_index = np.argmax(result)
+    # get the tag associated to the highest numerical value prediction
+    tag = classes[result_index]
+
+    # If percentage is greater then 70%
+    if result[result_index] > 0.7:
+        # we grab the list of responds and choose one at random
+        for tg in data["intents"]:
+            if tg["tag"] == tag:
+                responses = tg['responses']
+        print(random.choice(responses))
+
+    # Else, we print out a confused bot response
+    else:
+        print("Sorry, I didn' understand. Try asking again.")
+
+
+
+
+# ----- Main -----
 while True:
-        userString = input("You: ")
-        if userString.lower() == "quit":
-            break
-
-        results = model.predict([bag_of_words(userString, words)])[0]
-        result_index = np.argmax(results)
-        tag = classes[result_index]
-
-        if results[result_index] > 0.7:
-            for tg in data["intents"]:
-                if tg["tag"] == tag:
-                    responses = tg['responses']
-
-            print(random.choice(responses))
-        else:
-            print("Sorry, I didn' understand. Try asking again.")
+    userString = input("You: ")
+    chatBot(userString)
